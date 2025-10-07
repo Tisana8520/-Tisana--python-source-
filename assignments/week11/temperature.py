e14db70c89679ed3d4d9f43e252ca5098513f6ac
Tisana Rakchumkong <Tisana,r@ku.th>
2025-10-07 11:31:00 +0700

    week 11

--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -23,3 +23,26 @@ Lowest: 19 C
 
 """
     
+def get_tempratures():
+    x = [32,30,31,33,30,31,34]
+    return x
+
+def analyze_temps(temp.list):
+    average_temp = 0
+    sum = 0
+    for temp in temp_lst:
+        sum = sum + temp
+        average_temp = sum / 7
+    max_temp = max(temp_list)
+    min_temp = min(temp_list)
+    return (average_temp ,max_temp, min_temp)
+
+def display_analysis(avg,high,low):
+    print("Temperature Analysis for the Week: ")
+    print("average: %f C" % (avg))
+    print(f"Highest:{high} c")
+    print("lowest: %d c" % (low))
+
+temp_list = get_tempratures()
+resualt = analyze_temp(temp_list)
+display_analysis(result[0],result[1],result[2])
